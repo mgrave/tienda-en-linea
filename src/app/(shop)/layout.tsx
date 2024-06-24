cbac9f050ef241859ebbba4da0bf4610b386b96f
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2024-06-24 14:55:28 -0500

    creando pagina 404

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,9 +1,13 @@
+import { TopMenu } from "@/components";
+
  export default function ShopLayout({children}: {
     children: React.ReactNode;
  }) {
     return (
         //min-h-screen hace que abarque toda la pantalla
-      <main className="min-h-screen bg-red-500">
+      <main className="min-h-screen">
+        <TopMenu/>
+
         {children}
       </main>
     );
