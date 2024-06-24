40f3c2348a9ecc14770a99cf247b4e7cdc09a2d6
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2024-06-24 12:33:42 -0500

    comenzando top menu

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,12 +3,12 @@ import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <main className="">
+    <div className="">
       <h1>Hola mundo</h1>
       <h1 className={`${titleFont.className} font-bold`}>Hola mundo</h1>
       
       
       
-    </main>
+    </div>
   );
 }
