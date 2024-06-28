ae30b65965907700c23e216559cb8c509a6c3dc2
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2024-06-28 09:49:02 -0500

    grid de productos terminado

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,21 @@
-import { Title } from "@/components";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from '../../seed/seed';
 
+//este es el arreglo de productos semilla de la base de datos
+const products = initialData.products;
 
 
 export default function Home() {
   return (
    <>
-   <Title title="Tienda" subtitle="Todos los Productos" className="mb-2"></Title>
+   <Title 
+   title="Tienda" 
+   subtitle="Todos los Productos" 
+   className="mb-2">
+
+   </Title>
+
+   <ProductGrid products={products}></ProductGrid>
    </>
   )
 }
---
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -0,0 +1,18 @@
+import {Product} from '@/interfaces';
+
+interface Props {
+    products: Product[];
+}
+
+export const ProductGrid = ({products}: Props) => {
+    return (
+        <div className='grid grid-cols-2 sm:grid-cols-3 gap-3 mb-10'>
+            {
+                products.map(product => (
+                    <span key={product.slug}>{product.title}</span>
+                ))
+            }
+        </div>
+    )
+}
+
