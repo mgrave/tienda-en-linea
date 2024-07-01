c28a4a3507e0cc2cfed8d4e7cffb051eccb53298
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2024-07-01 13:14:36 -0500

    cambiar imagen mosue-over

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,22 +1,31 @@
+'use client';
+
 import {Product} from '@/interfaces';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 interface Props {
     product: Product;
 }
 
 export const ProductGridItem = ({product}: Props) => {
+
+        //nuestro estado inicial
+        const [displayImage, setDisplayImage] = useState(product.images[0]);
+
     return (
         //el overflow hiden es para que corte las partes que se salgan de las imagenes.
         <div className='rounded-md overflow-hidden fade-in'>
             <Link href={`/product/${product.slug} `}>
             <Image
-            src={`/products/${product.images[0]}`}
+            src={`/products/${displayImage}`}
             alt={product.title}
-            className='w-full object-cover'
+            className='w-full object-cover rounded'
             width={500}
             height={500}
+            onMouseEnter={() => setDisplayImage(product.images[1])}
+            onMouseLeave={() => setDisplayImage(product.images[0]) }
             >
             </Image>
             
