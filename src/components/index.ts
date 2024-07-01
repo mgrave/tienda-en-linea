cda74854ad3fe6ff95e06afbaf89ad0c60eb9ad7
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2024-07-01 12:28:19 -0500

    Tarjeta de producto lista

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './products/product-grid/ProductGrid';
+export * from './products/product-grid/ProductGridItem';
 
 export * from './ui/top-menu/TopMenu';
 export * from './ui/not-found/PageNotFound';
