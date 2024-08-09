039443fc23cc4e8aa3da4fa48e33342282a3b317
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2024-08-08 19:20:00 -0500

    colocar orden productos y cantidad

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth.config";
 import type { Address, Size } from "@/interfaces";
+import prisma from "@/lib/prisma";
 
 interface ProductToOrder {
     productId: string;
@@ -14,14 +15,27 @@ export const  placeOrder = async(producIds: ProductToOrder[], address: Address) 
     const session = await auth();
     const userId = session?.user.id;
 
+    //verificar session de usuario
+
     if (!userId) {
         return {
             ok: false,
             message: 'No hay sesión de usuario'
         }
     }
-
-    console.log({producIds, address, userId});
+    //Obtener la informacion de los productos
+    //nota: podemos llevar mas de 2 productos  con el mismo ID
+    const products = await prisma.product.findMany({
+        where: {
+            id: {
+                in: producIds.map(p => p.productId)
+            }
+        }
+    });
+    //calcular los montos
+    const itemsInOrder = producIds.reduce((count, p) =>count + p.quantity , 0);
+    console.log(itemsInOrder);
+  
 
 
 }
