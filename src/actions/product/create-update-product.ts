a9200f6045675ff9e9f4a5769aec69dcc01598c1
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2024-08-18 22:25:12 -0500

    cargando a claudinary

--- src/actions/product/create-update-product.ts
+++ src/actions/product/create-update-product.ts
@@ -4,6 +4,8 @@ import prisma from '@/lib/prisma';
 import { Gender, Product, Size } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
 import { z } from 'zod';
+import { v2 as cloudinary } from 'cloudinary';
+cloudinary.config(process.env.CLOUDINARY_URL ?? '');
 
 const productSchema = z.object({
     id: z.string().uuid().optional().nullable(),
@@ -76,7 +78,8 @@ export const createUpdateProduct = async(formData: FormData ) => {
             //Proceso de carga  y guardado de imagenes
             //recorrer las imagenes y guardarlas
             if (formData.getAll('images')){
-                console.log(formData.getAll('images'));
+              const images = await uploadImages(formData.getAll('images') as File[]);
+              console.log(images);
             }
     
             return {
@@ -102,6 +105,34 @@ export const createUpdateProduct = async(formData: FormData ) => {
             ok: false,
             message: 'Revisar los logs, no se pudo actualizar/crear'
         }
+    }
+
+}
+
+const uploadImages = async(images: File[]) => {
+
+    try {
+        
+        const uploadPromises = images.map( async(image) => {
+            try {
+                const buffer = await image.arrayBuffer();
+                const base64Image = Buffer.from(buffer).toString('base64');
+    
+                return cloudinary.uploader.upload(`data:image/png;base64,${ base64Image }`)
+                .then( r => r.secure_url);
+                
+            } catch (error) {
+                console.log(error);
+                return null;        
+            }
+        })
+
+        const uploadedImages = await Promise.all(uploadPromises);
+        return uploadedImages;
+
+    } catch (error) {
+        console.log(error);
+        return null;
     }
 
 }
